bf32d117b91641a404b92b7608cd4898cc027d6f
Nitu Balwani <balwaninitu@gmail.com>
2021-07-13 11:13:10 +0800

    Methods void and return params

--- Java_basics/src/basics/LearningMethods.java
+++ Java_basics/src/basics/LearningMethods.java
@@ -4,14 +4,13 @@ public class LearningMethods {
 
     public static void main(String[] args) {
         System.out.println("heheh");
-        printSomeStuff(55);
+        MyUtils.printSomeStuff(55);
+        MyUtils.sumTwoNumbers(5,7);
+        int myVar = MyUtils.add10(99);
+        System.out.println(myVar);
+
 
     }
 
-    public static void printSomeStuff(String argument){
-        System.out.println("Gibberish bla bla " + argument);
-    }
-    public static void printSomeStuff(int argument){
-        System.out.println("Integer parsed in " + argument);
-    }
+
 }
--- Java_basics/src/basics/MyUtils.java
+++ Java_basics/src/basics/MyUtils.java
@@ -1,4 +1,21 @@
 package basics;
 
 public class MyUtils {
+
+    public static void printSomeStuff(String argument){
+        System.out.println("Gibberish bla bla " + argument);
+    }
+    public static void printSomeStuff(int argument){
+        System.out.println("Integer parsed in " + argument);
+    }
+
+    public static void sumTwoNumbers(int firstNum, int secondNum){
+        System.out.println(firstNum+secondNum);
+    }
+
+    public static int add10(int someNum){
+       int result = someNum + 10;
+       return result;
+
+    }
 }
