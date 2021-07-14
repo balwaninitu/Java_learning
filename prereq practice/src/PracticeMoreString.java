db6233a73e6732a34a598a353fc73769b0103e2b
Nitu Balwani <balwaninitu@gmail.com>
2021-07-14 16:09:37 +0800

    indexof

---
+++ prereq practice/src/PracticeMoreString.java
@@ -0,0 +1,13 @@
+public class PracticeMoreString {
+
+    public static void main(String[] args) {
+
+        String str = "Hello there Modi";
+
+        int a = str.indexOf("there");
+
+        System.out.println(a);
+
+
+    }
+}
