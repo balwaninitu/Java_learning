9948d5160125a9f34289fdc2441561f3faaf11df
Nitu Balwani <balwaninitu@gmail.com>
2021-07-14 15:36:11 +0800

    String

--- Prerequisite_OOP/src/Zoo.java
+++ Prerequisite_OOP/src/Zoo.java
@@ -3,18 +3,21 @@ public class Zoo {
 
     public static void main(String[] args) {
 
+        Flyable flyingBird = new Sparrow("pinu", "f", 4,2);
+        flyingBird.fly();
+
         Animal sparrow1 = new Sparrow("Chinu", "f", 2, 6);
-        sparrow1.move();
+        //sparrow1.move();
 
         Animal fish1 = new Fish("Dory", "f", 7,3);
-        fish1.move();
+       // fish1.move();
 
-        moveAnimals(fish1);
-        moveAnimals(sparrow1);
+        moveAnimal(fish1);
+        moveAnimal(sparrow1);
 
     }
 
-    public static void moveAnimals(Animal animal){
+    public static void moveAnimal(Animal animal){
         animal.move();
     }
 }
