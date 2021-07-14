b478924788e4f1a5ff3e58e5f359e3a3ccd05957
Nitu Balwani <balwaninitu@gmail.com>
2021-07-14 16:21:39 +0800

    while loop

---
+++ prereq practice/src/practice/loops/LoopsPractice.java
@@ -0,0 +1,15 @@
+package practice.loops;
+
+public class LoopsPractice {
+
+    public static void main(String[] args) {
+
+        //while loop
+        int count = 0;
+
+        while(count <=10){
+            System.out.println("Nitu");
+            count = count +1;
+        }
+    }
+}
