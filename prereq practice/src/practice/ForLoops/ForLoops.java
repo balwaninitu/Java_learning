d322a52f684e92109a92082bfdb709c09bc20ae1
Nitu Balwani <balwaninitu@gmail.com>
2021-07-19 13:38:46 +0800

    Nested loop

--- prereq practice/src/practice/ForLoops/ForLoops.java
+++ prereq practice/src/practice/ForLoops/ForLoops.java
@@ -13,6 +13,26 @@ String name = "fsdgfdghdfhfghghghghdfgioi:";
             System.out.println("i:"+i);
         }
 
+        //debugging loop
+        int counter = 0;
+        for (int i= 0; i <= 100; i=i+1){
+            System.out.println(i);
+            int temp = counter +1;
+            counter = temp;
+        }
+        int counter1 =0;
+        for (int idx=0; idx<100; idx++){
+
+            for (int j=0; j<10; j++){
+                for (int i=0; i <100; i++)
+                System.out.println("the value of idx was "+idx + "------" +j + "----"+i);
+                counter1++;
+
+            }
+
+        }
+        System.out.println("Counter1" + counter1);
+
     }
 
     }
