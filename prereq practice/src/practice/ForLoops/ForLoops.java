1cddd51701b6b272ffdbddbdfbf39d62034943c0
Nitu Balwani <balwaninitu@gmail.com>
2021-07-19 12:45:53 +0800

    For loop

--- prereq practice/src/practice/ForLoops/ForLoops.java
+++ prereq practice/src/practice/ForLoops/ForLoops.java
@@ -4,9 +4,9 @@ public class ForLoops {
 
     public static void main(String[] args) {
 
-
-        for (int i = 0; i < 100; i++){
-            System.out.println("i: "+ i);
+String name = "fsdgfdghdfhfghghghghdfgioi:";
+        for (int i = 0; i < name.length(); i++){
+            System.out.println("char: "+ name.charAt(i));
         }
 
     }
