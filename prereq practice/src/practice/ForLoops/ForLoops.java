8a22b612d018af32866fcc78bfb0c9fec16db902
Nitu Balwani <balwaninitu@gmail.com>
2021-07-19 13:00:48 +0800

    for loop even nums

--- prereq practice/src/practice/ForLoops/ForLoops.java
+++ prereq practice/src/practice/ForLoops/ForLoops.java
@@ -5,9 +5,15 @@ public class ForLoops {
     public static void main(String[] args) {
 
 String name = "fsdgfdghdfhfghghghghdfgioi:";
-        for (int i = 0; i <= name.length() -1; i++){
+        for (int i = name.length()-1 ; i >= 0; i--){
             System.out.println("char: "+ name.charAt(i));
+        }
+//print even numbers
+        for (int i = 0; i <= 100; i+=2){
+            System.out.println("i:"+i);
         }
 
     }
-}
+
+    }
+
