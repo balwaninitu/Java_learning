1dc9adbaf92a399e915c610095555f13f01112ac
Nitu Balwani <balwaninitu@gmail.com>
2021-07-20 12:37:14 +0800

    read from file

--- ProcessingFiles/src/Application.java
+++ ProcessingFiles/src/Application.java
@@ -1,16 +1,26 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        for (int i=0; i <=3; i++){
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter some text: ");
-            String enteredText = input.nextLine();
-            System.out.println(enteredText);
+//        for (int i=0; i <=3; i++){
+//            Scanner input = new Scanner(System.in);
+//            System.out.println("Enter some text: ");
+//            String enteredText = input.nextLine();
+//            System.out.println(enteredText);
+  //      }
+
+        File file = new File("myfile.txt");
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            System.out.println(line);
         }
+        input.close();
+
 
 
     }
