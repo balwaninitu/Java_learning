8668e34cc2cda57ce5adee302e24f9beee16532e
Nitu Balwani <balwaninitu@gmail.com>
2021-07-21 14:06:43 +0800

    LinkedHashSet

--- ProcessingFiles/src/lesson3/Application.java
+++ ProcessingFiles/src/lesson3/Application.java
@@ -1,13 +1,15 @@
 package lesson3;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class Application {
 
     public static void main(String[] args) {
         /*set is used to prevent duplicates, if there is duplicate value
         it will going to print only once if it get changed to arrayList all
-        duplicates will get printed. Set is unordered
+        duplicates will get printed. Set is unordered, ordered can be maintaine
+        by LinkedHasSet
          */
         HashSet<Integer> values = new HashSet<Integer>();
         values.add(12);
@@ -19,6 +21,16 @@ public class Application {
         for(Integer value : values){
             System.out.println(value);
         }
+
+        LinkedHashSet<String> str = new LinkedHashSet<String>();
+        str.add("Hi");
+        str.add("Hola");
+        str.add("Bonjour");
+        str.add("Hola");
+        str.add("Hello");
+        str.add("Namaste");
+        str.add("Namaste");
+        System.out.println(str);
 
 
     }
