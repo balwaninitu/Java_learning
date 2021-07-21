4046434740926613d0af663e046829d8b273bee3
Nitu Balwani <balwaninitu@gmail.com>
2021-07-21 14:49:58 +0800

    Animal class

--- ProcessingFiles/src/lesson3/Application.java
+++ ProcessingFiles/src/lesson3/Application.java
@@ -30,7 +30,41 @@ public class Application {
         str.add("Hello");
         str.add("Namaste");
         str.add("Namaste");
+
         System.out.println(str);
+
+        for (String strs : str){
+            System.out.println(strs);
+        }
+
+        HashSet<Animal> animals = new HashSet<Animal>();
+
+        Animal animal1 = new Animal("Dog", 12);
+        Animal animal2 = new Animal("Cat", 10);
+        Animal animal3 = new Animal("Bird", 9);
+        Animal animal4 = new Animal("Dog", 12);
+        Animal animal5 = new Animal("Bear", 7);
+
+        animals.add(animal1);
+        animals.add(animal2);
+        animals.add(animal3);
+        animals.add(animal4);
+        animals.add(animal5);
+
+        System.out.println(animal1.equals(animal4));
+
+        System.out.println(animal1.hashCode());
+        System.out.println(animal4.hashCode());
+
+
+        for (Animal animal : animals){
+            System.out.println(animal);
+        }
+
+
+
+
+
 
 
     }
---
+++ ProcessingFiles/src/lesson4/Application.java
@@ -0,0 +1,9 @@
+package lesson4;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+    }
+}
