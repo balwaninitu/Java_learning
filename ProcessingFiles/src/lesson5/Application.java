edbdec44b9f3969e3691bdc9603cf731ac6f6297
Nitu Balwani <balwaninitu@gmail.com>
2021-07-22 17:38:06 +0800

    Treemap

--- ProcessingFiles/src/lesson5/Application.java
+++ ProcessingFiles/src/lesson5/Application.java
@@ -2,24 +2,37 @@ package lesson5;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Application {
 
     public static void main(String[] args) {
 //hashmap implements the map interface which have key and value
         //hashmap in unordered whereas linkedhashmap is ordered
-        LinkedHashMap<String, String> dictionary = new LinkedHashMap<String, String>();
+        //LinkedHashMap<String, String> dictionary = new LinkedHashMap<String, String>();
         //HashMap<String, String> dictionary = new HashMap<String, String>();
-        dictionary.put("Brave", "ready to face and endure danger or pain");
+
+        //Treemap sort keys in natural order like alphabetically for string
+        TreeMap<String, String> dictionary = new TreeMap<String, String>();
+
+
         dictionary.put("Brilliant", "exceptionally clever or talented");
         dictionary.put("Joy", "a feeling of great pleasure and happiness");
         dictionary.put("Confidence", "the state of feeling certain about the truth of something.");
 
-        for (String word: dictionary.keySet()){
-            //print values
-            System.out.println(dictionary.get(word));
-            //print keys
-            //System.out.println(word);
+//        for (String word: dictionary.keySet()){
+//            //print values
+//            System.out.println(dictionary.get(word));
+//            //print keys
+//            //System.out.println(word);
+//
+//        }
+
+        //loop over key and value both
+        for(Map.Entry<String,String> entry:dictionary.entrySet()){
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
         }
 
     }
